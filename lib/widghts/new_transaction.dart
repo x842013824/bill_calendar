fe4787d1e0f76dadec048b1c42b6b24f36be8add
x842013824 <44201099+x842013824@users.noreply.github.com>
2024-01-23 14:35:15 +0800

    NewTransaction转化为StatefulWight
    1. 支持完成所有输入后，自动提交
    2. 支持自动撤回底部表单

--- lib/widghts/new_transaction.dart
+++ lib/widghts/new_transaction.dart
@@ -1,43 +1,57 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final _titleController = TextEditingController();
-  final _amountController = TextEditingController();
-
+class NewTransaction extends StatefulWidget {
   final ValueChanged<({String title, double amount})> onSubmit;
 
-  NewTransaction(this.onSubmit, {super.key});
+  const NewTransaction(this.onSubmit, {super.key});
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final _titleController = TextEditingController();
+
+  final _amountController = TextEditingController();
+
+  void _submit() {
+    final title = _titleController.text;
+    final amount = double.parse(_amountController.text);
+    if (title.isNotEmpty && amount > 0) {
+      widget.onSubmit((title: title, amount: amount));
+      Navigator.of(context).pop();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          TextField(
-            decoration: const InputDecoration(labelText: 'Title'),
-            controller: _titleController,
-          ),
-          TextField(
-            decoration: const InputDecoration(labelText: 'Amount'),
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-          ),
-          TextButton(
-            onPressed: () {
-              final title = _titleController.text;
-              final amount = double.parse(_amountController.text);
-              if (title.isNotEmpty && amount > 0) {
-                onSubmit((title: title, amount: amount));
-              }
-            },
-            child: const Text(
-              'Add Transaction',
-              style: TextStyle(color: Colors.purple),
+      child: Container(
+        constraints: const BoxConstraints(maxHeight: 320),
+        margin: const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              onSubmitted: (_) => _submit(),
             ),
-          )
-        ],
+            TextField(
+              decoration: const InputDecoration(labelText: 'Amount'),
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => _submit(),
+            ),
+            TextButton(
+              onPressed: _submit,
+              child: const Text(
+                'Add Transaction',
+                style: TextStyle(color: Colors.purple),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
